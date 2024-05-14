d8a633bd3b25849a69a35497cb66b60c34a74d21
Lasse <lap@di.ku.dk>
2024-05-14 22:17:37 +0200

    use correct ethertype for arp

--- handlers/arp.c
+++ handlers/arp.c
@@ -1,6 +1,7 @@
 #include <arpa/inet.h>
 
 #include <rte_malloc.h>
+#include <rte_mbuf.h>
 
 #include "handlers/arp.h"
 #include "handlers/ethernet.h"
@@ -20,6 +21,8 @@ void arp_init_handler(struct handler_t* handler) {
 
 uint16_t arp_read(struct rte_mbuf* buffer, struct interface_t* interface, void* priv) {
     RTE_LOG(INFO, USER1, "ARP read handler called.\n");   
+
+    struct arp_header_t* header = rte_pktmbuf_mtod(buffer, struct arp_header_t*);
 }
 
 struct handler_t* arp_create_handler() {
@@ -30,5 +33,5 @@ struct handler_t* arp_create_handler() {
 
     handler->operations.read = arp_read;
 
-    ADD_TO_PRIORITY(&ethernet_type_to_handler, htons(0x86dd), handler);
+    ADD_TO_PRIORITY(&ethernet_type_to_handler, htons(0x0806), handler);
 }
